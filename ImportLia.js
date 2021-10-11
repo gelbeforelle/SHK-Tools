41f7f2b1007790901bb431c53f48c033a47a4b49
gelbeforelle <niclas.k1656@gmail.com>
2021-10-11 15:23:16 +0200

    Outsource QTI Output into functions +

--- ImportLia.js
+++ ImportLia.js
@@ -9,6 +9,7 @@ function idGen(){
  return "id-TUBAF-" + Date.now();
 }
 
+
 const xmlns = "http://www.imsglobal.org/xsd/imsqti_v2p1";
 
 var currType = taskType.text;
@@ -36,104 +37,203 @@ class Test {
         this.tasks = [];
         this.valid = false;
     }
+
+    parseResponse(lastType, i, xmlDoc, main){
+        var response;
+        if(this.tasks[i].type != taskType.paragraph) if(this.tasks[i].type != lastType || this.tasks[i].type == taskType.text){
+            
+            lastType = this.tasks[i].type;
+            response = xmlDoc.createElement("responseDeclaration");
+            response.setAttribute("identifier", "RESPONSE_"+i);
+
+            let cardinality;
+            if(this.tasks[i].type == taskType.multipleChoice) cardinality = "multiple";
+            else cardinality = "single";
+            response.setAttribute("cardinality", cardinality);
+
+            if(this.tasks[i].type == taskType.text){
+                let correct = xmlDoc.createElement("correctResponse");
+                let value = xmlDoc.createElement("value");
+                value.innerHTML = this.tasks[i].text;
+                correct.appendChild(value);
+                response.appendChild(correct);
+            }
+
+        } if((this.tasks[i].type == taskType.singleChoice || this.tasks[i].type == taskType.multipleChoice) && this.tasks[i].isCorrect) {
+            if(response) if(response.getElementsByTagName("correctResponse").length<=0){
+                response.appendChild(xmlDoc.createElement("correctResponse"));
+                console.log("Created missing correctResponse!");
+            }
+            
+            let value = xmlDoc.createElement("value");
+            value.innerHTML = "ID_" + i;
+            console.log("Data added:");
+            console.log(value.innerText);
+            console.log(this.tasks[i].text);
+
+            if(response) response.getElementsByTagName("correctResponse")[0].appendChild(value);
+        }
+        if(response) main.appendChild(response);
+        return { lastType : lastType, i : i, main : main} ;
+    }
+
+    parseTask(xmlDoc, body, currParagraph, i){
+        let entry;
+        switch(this.tasks[i].type){
+            case taskType.text : entry = xmlDoc.createElement("textEntryInteraction");
+                                 entry.setAttribute("responseIdentifier", "RESPONSE_"+i);
+                                 console.log("Text entry: ", entry.outerHTML);
+                                 currParagraph.innerHTML+=entry.outerHTML;
+                                 console.log(this.tasks[i]);
+                                 i++;
+                                 
+                                 break;
+            case taskType.multipleChoice : case taskType.singleChoice : if(currParagraph.innerHTML) body.appendChild(currParagraph);
+                                           currParagraph = xmlDoc.createElement("p");
+                                           entry = xmlDoc.createElement("choiceInteraction");
+                                           entry.setAttribute("responseIdentifier", "RESPONSE_"+i);
+                                           for(; i<this.tasks.length; i++){
+                                            if(!(this.tasks[i].type == taskType.singleChoice || this.tasks[i].type == taskType.multipleChoice)) break;
+                                            console.log("Adding to MC/ SC: ", this.tasks[i].text);
+                                            let choice = xmlDoc.createElement("simpleChoice");
+                                            choice.setAttribute("identifier", "ID_"+i);
+                                            let p = xmlDoc.createElement("p");
+                                            p.innerHTML=this.tasks[i].text;
+                                            choice.appendChild(p);
+                                            entry.appendChild(choice);
+                                           }
+                                           body.appendChild(entry);
+                                           console.log(this.tasks[i]);
+                                           
+                                           break;
+            case taskType.paragraph : if(this.tasks[i].isCorrect && this.tasks[i].text){
+                                       body.appendChild(currParagraph);
+                                       currParagraph = xmlDoc.createElement("p");
+                                       currParagraph.innerHTML = this.tasks[i].text;
+                                      } else {
+                                        currParagraph.innerHTML += this.tasks[i].text;
+                                      }
+                                      console.log(this.tasks[i]);
+                                      i++;
+                                      
+                                      break;
+            default: console.log("Something went wrong.... ", this.tasks[i].type); i++; break;
+
+        }
+        return i;
+    }
+
     addTask(task){
         this.tasks[this.tasks.length] = task;
         if(task.type != taskType.paragraph){
+
              this.valid = true;
              
         }
     }
+    toQTI2(){
+        let xmlDoc = document.implementation.createDocument("", "", null);
+       
+        var body = xmlDoc.createElement("itemBody");
+        var currParagraph=xmlDoc.createElement("p");
+        var main = xmlDoc.createElement("assessmentItem");
+        
+        for(let i=0, lastType=taskType.paragraph, lastIndex=0; i<this.tasks.length; i++){
+            for(;i<this.tasks.length; i++){
+                if(this.tasks[i].type != lastType && this.tasks[i].type != taskType.paragraph){
+                    main.append(body);
+                
+                    download("Test of alternative parser", main.outerHTML);
+                    console.log("IMPORTANT#######################");
+                    console.log(main);
+                    
+                    body = xmlDoc.createElement("itemBody");
+                    currParagraph=xmlDoc.createElement("p");
+                    main = xmlDoc.createElement("assessmentItem");
+                    break;
+                }
+                
+
+                var returned = this.parseResponse(lastType, i, xmlDoc, main);
+
+                if(this.tasks[i].type != taskType.paragraph) i = this.parseTask(xmlDoc, body, currParagraph, i);
+                
+                main = returned.main;
+                lastType = returned.lastType;
+
+                if(i<this.tasks.length && this.tasks[i].type == taskType.paragraph){
+                    for(;i<this.tasks.length && this.tasks[i].type == taskType.paragraph; i++){
+                        currParagraph.innerHTML += this.tasks[i].text;
+                        
+                        if(this.tasks[i].isCorrect) break;
+                    }
+                }
+                if(currParagraph.innerHTML) body.appendChild(currParagraph);
+
+                
+            }
+
+           /* if(this.tasks[i].type != lastType && this.tasks[i].type != taskType.paragraph){
+                // Doc parser
+                
+                //if(currParagraph.innerHTML) body.appendChild(currParagraph);
+                
+                
+                lastIndex=i;
+                
+                
+                
+                
+            
+                
+                
+                
+
+                
+                
+                
+                
+            } else if(this.tasks[i].type == taskType.paragraph){
+                if(this.tasks[i].isCorrect && this.tasks[i].text){
+                    if(currParagraph) body.appendChild(currParagraph);
+                    currParagraph = xmlDoc.createElement("p");
+                    currParagraph.innerHTML = this.tasks[i].text;
+                   } else {
+                     currParagraph.innerHTML += this.tasks[i].text;
+                   }
+                   console.log(this.tasks[i]);
+                   
+
+
+            }
+            */
+        }
+    }
+
     toQTI(){
         var xmlDoc = document.implementation.createDocument("", "", null);
         var main = xmlDoc.createElement("assessmentItem");
         main.setAttribute("title", this.title);
         main.setAttribute("identifier", idGen());
         main.setAttribute("xmlns", xmlns);
-        var taskcopy = this.tasks;
+        
         var response;
+
         for(let i=0, lastType=taskType.paragraph; i<this.tasks.length; i++){
-            
-            if(this.tasks[i].type != taskType.paragraph) if(this.tasks[i].type != lastType || this.tasks[i].type == taskType.text){
-                if(response) main.appendChild(response);
-                lastType = this.tasks[i].type;
-                response = xmlDoc.createElement("responseDeclaration");
-                response.setAttribute("identifier", "RESPONSE_"+i);
-
-                let cardinality;
-                if(this.tasks[i].type == taskType.multipleChoice) cardinality = "multiple";
-                else cardinality = "single";
-                response.setAttribute("cardinality", cardinality);
-
-                if(this.tasks[i].type == taskType.text){
-                    let correct = xmlDoc.createElement("correctResponse");
-                    let value = xmlDoc.createElement("value");
-                    value.innerHTML = this.tasks[i].text;
-                    correct.appendChild(value);
-                    response.appendChild(correct);
-                }
-
-            } if((this.tasks[i].type == taskType.singleChoice || this.tasks[i].type == taskType.multipleChoice) && this.tasks[i].isCorrect) {
-                if(response.getElementsByTagName("correctResponse").length<=0){
-                    response.appendChild(xmlDoc.createElement("correctResponse"));
-                    console.log("Created missing correctResponse!");
-                }
-                
-                let value = xmlDoc.createElement("value");
-                value.innerHTML = "ID_" + i;
-                console.log("Data added:");
-                console.log(value.innerText);
-                console.log(this.tasks[i].text);
-
-                response.getElementsByTagName("correctResponse")[0].appendChild(value);
-            }
+            let returned = this.parseResponse(lastType, i, xmlDoc, main);
+            lastType = returned.lastType;
+            i = returned.i;
+            main = returned.main;
         }
+
+        //new function here!
         if(response) main.appendChild(response);
         let body = xmlDoc.createElement("itemBody");
         let currParagraph=xmlDoc.createElement("p");
         for(let i=0; i<this.tasks.length;){
-            let entry;
+            i = this.parseTask(xmlDoc, body, currParagraph, i);
             console.log("Starting switch... ", i);
-            switch(this.tasks[i].type){
-                case taskType.text : entry = xmlDoc.createElement("textEntryInteraction");
-                                     entry.setAttribute("responseIdentifier", "RESPONSE_"+i);
-                                     console.log("Text entry: ", entry.outerHTML);
-                                     currParagraph.innerHTML+=entry.outerHTML;
-                                     console.log(this.tasks[i]);
-                                     i++;
-                                     
-                                     break;
-                case taskType.multipleChoice : case taskType.singleChoice : if(currParagraph.innerHTML) body.appendChild(currParagraph);
-                                               currParagraph = xmlDoc.createElement("p");
-                                               entry = xmlDoc.createElement("choiceInteraction");
-                                               entry.setAttribute("responseIdentifier", "RESPONSE_"+i);
-                                               for(; i<this.tasks.length; i++){
-                                                if(!(this.tasks[i].type == taskType.singleChoice || this.tasks[i].type == taskType.multipleChoice)) break;
-                                                console.log("Adding to MC/ SC: ", this.tasks[i].text);
-                                                let choice = xmlDoc.createElement("simpleChoice");
-                                                choice.setAttribute("identifier", "ID_"+i);
-                                                let p = xmlDoc.createElement("p");
-                                                p.innerHTML=this.tasks[i].text;
-                                                choice.appendChild(p);
-                                                entry.appendChild(choice);
-                                               }
-                                               body.appendChild(entry);
-                                               console.log(this.tasks[i]);
-                                               
-                                               break;
-                case taskType.paragraph : if(this.tasks[i].isCorrect && this.tasks[i].text){
-                                           body.appendChild(currParagraph);
-                                           currParagraph = xmlDoc.createElement("p");
-                                           currParagraph.innerHTML = this.tasks[i].text;
-                                          } else {
-                                            currParagraph.innerHTML += this.tasks[i].text;
-                                          }
-                                          console.log(this.tasks[i]);
-                                          i++;
-                                          
-                                          break;
-                default: console.log("Something went wrong.... ", this.tasks[i].type); i++; break;
-
-            }
+            
         }
         if(currParagraph.innerHTML) body.appendChild(currParagraph);
         main.appendChild(body);
@@ -264,4 +364,5 @@ function parsePage(text, test){
         download(test.title, content.childNodes[0].outerHTML);
     }
     console.log(test);
+    test.toQTI2();
 }
